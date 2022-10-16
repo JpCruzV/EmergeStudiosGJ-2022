d0d55cc5d8c9173a64949ba0af85989374c0f364
JpCruzV <88095820+JpCruzV@users.noreply.github.com>
2022-10-16 08:22:40 -0600

    asdfasdf

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -7,14 +7,14 @@ public class MoveCamera : MonoBehaviour {
     [SerializeField] GameObject playerRef;
     [HideInInspector] public bool onPlayer = false;
     Vector3 playerPos;
-    bool stopCinematic = false;
     [HideInInspector] public bool teleportedToPlayer = false;
 
 
+    float posx = 0;
+    float posy = 0;
+
     private void Update() {
 
-
-        playerPos = new Vector3(playerRef.transform.position.x, playerRef.transform.position.y, -10);
 
         if (playerRef.GetComponent<PlayerController>().hasTeleported == true && teleportedToPlayer == false) {
 
@@ -22,28 +22,39 @@ public class MoveCamera : MonoBehaviour {
             transform.position = new Vector3(playerPos.x , playerPos.y);
         }
 
-        if(transform.position != playerPos) {
-
-            if (playerPos.y > -5)
-                transform.position = Vector3.MoveTowards(transform.position, playerPos, Time.deltaTime * 50);
 
 
-            else if (playerPos.y <= -5) {
-                Vector3 newPlayerPos = new Vector3(playerRef.transform.position.x, 2, -10);
-                transform.position = Vector3.MoveTowards(transform.position, newPlayerPos, Time.deltaTime * 50);
-            }
+        if (playerRef.transform.position.y <= -2)
+        {
+
+            posy = 2;
         }
 
+        else if (playerRef.transform.position.y >= 50)
+        {
 
-        if (this.transform.position == playerPos && stopCinematic == false) {
-
-            stopCinematic = true;
-            onPlayer = true;
+            posy = 50;
         }
 
-        else {
+        else
+            posy = playerRef.transform.position.y;
 
-            Vector3.Lerp(transform.position, playerPos, 10);
+
+        if (playerRef.transform.position.x <= -18)
+        {
+
+            posx = -18;
         }
+
+        else if (playerRef.transform.position.x >= 18)
+        {
+            posx = 18;
+        }
+
+        else
+            posx = playerRef.transform.position.x;
+
+        playerPos = new Vector3(posx, posy, -10);
+        transform.position = Vector3.MoveTowards(transform.position, playerPos, Time.deltaTime * 50);
     }
 }
